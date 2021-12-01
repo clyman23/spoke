89cdbdc533cb55fdf39f91187430f947b90b4cad
Max Dumas <maltor124@gmail.com>
2021-11-30 20:16:14 -0500

    Use helpers to simplify heatmap generation

---
+++ spoke/datasets.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import geopandas as gpd
+import osmnx as ox
+
+def load_consolidated_mnh_below_34th(show=False):
+    G = ox.io.load_graphml('../../data/consolidated_mnh_below_34th.graphml')
+    if show:
+        ox.plot.plot_graph(G)
+    return G
+
+def load_normalized_crash_gdf(with_nodes=True):
+    if with_nodes:
+        df = pd.read_csv('../../data/crash_data_normalized_with_node_graph.csv')
+    else:
+        df = pd.read_csv('../../data/crash_data_normalized.csv')
+    return gpd.GeoDataFrame(data=df, geometry=gpd.points_from_xy(df.LONGITUDE, df.LATITUDE), crs='EPSG:4326')
+
+def load_normalized_crash_gdf_with_nodes():
+    df = pd.read_csv('../../data/crash_data_normalized.csv')
+    return gpd.GeoDataFrame(data=df, geometry=gpd.points_from_xy(df.LONGITUDE, df.LATITUDE), crs='EPSG:4326')
+
+def load_danger_df():
+    return pd.read_pickle('../../data/consolidated_danger_by_node_id.pkl.gz')
