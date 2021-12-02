776339500c34d3dc595aa8763d364b676da7a5a8
Max Dumas <maltor124@gmail.com>
2021-12-02 16:49:21 -0500

    Add heatmap generation and other nice stuff

--- spoke/data_processing/03_joining_crash_data_to_nodes.py
+++ spoke/data_processing/03_joining_crash_data_to_nodes.py
@@ -1,21 +1,13 @@
-import sys
-from os.path import abspath, join
-
 import osmnx as ox
 import pandas as pd
 from tqdm import tqdm
 
-DIR = sys.path[0] if __name__ == '__main__' else dirname(__loader__.path)
-
-module_path = abspath(join(DIR, '../..'))
-if module_path not in sys.path:
-    sys.path.append(module_path)
-
 from spoke.graphing import saturate_nodes
 
-INPUT_FILE_GRAPH = abspath(join(DIR, '../../pipeline_data/target_map.graphml'))
-INPUT_FILE_CRASHES = abspath(join(DIR, '../../pipeline_data/crash_data_normalized.pkl.gz'))
-OUTPUT_FILE = abspath(join(DIR, '../../pipeline_data/crash_data_normalized_with_node_graph.pkl.gz'))
+INPUT_FILE_GRAPH = "pipeline_data/target_map.graphml"
+INPUT_FILE_CRASHES = "pipeline_data/crash_data_normalized.pkl.gz"
+OUTPUT_FILE = "pipeline_data/crash_data_normalized_with_node_graph.pkl.gz"
+
 
 def process():
     # We load the graph and the normalized set of crashes
@@ -25,27 +17,32 @@ def process():
     # This is the distance from a crash to its nearest node beyond which we assume
     # that the crash occurred outside of our graph and isn't really matched to that node.
     THRESHOLD_DIST_M = 100
-    
+
     # Now we create a dataframe with a row for each crash that contains the ID
     # of the nearest street network node.
-    nearest_nodes = pd.DataFrame(columns=['NODE_ID', 'NODE_DIST_FROM_CRASH_M', 'NODE_LATITUDE', 'NODE_LONGITUDE'])
+    nearest_nodes = pd.DataFrame(
+        columns=["NODE_ID", "NODE_DIST_FROM_CRASH_M", "NODE_LATITUDE", "NODE_LONGITUDE"]
+    )
 
     for i, crash in tqdm(crash_df.iterrows(), total=crash_df.shape[0]):
-        nn_id, dist = ox.distance.nearest_nodes(G, crash.LONGITUDE, crash.LATITUDE, return_dist=True)
+        nn_id, dist = ox.distance.nearest_nodes(
+            G, crash.LONGITUDE, crash.LATITUDE, return_dist=True
+        )
         if dist > THRESHOLD_DIST_M:
             continue
         nn = G.nodes[nn_id]
         nearest_nodes.loc[i] = {
-            'NODE_ID': nn_id,
-            'NODE_DIST_FROM_CRASH_M': dist,
-            'NODE_LATITUDE': nn['y'],
-            'NODE_LONGITUDE': nn['x'],
+            "NODE_ID": nn_id,
+            "NODE_DIST_FROM_CRASH_M": dist,
+            "NODE_LATITUDE": nn["y"],
+            "NODE_LONGITUDE": nn["x"],
         }
-    
-    crash_df_with_nodes = pd.concat((crash_df, nearest_nodes), join='inner', axis=1)
-    
+
+    crash_df_with_nodes = pd.concat((crash_df, nearest_nodes), join="inner", axis=1)
+
     return crash_df_with_nodes
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     df = process()
     df.to_pickle(OUTPUT_FILE)
--- spoke/data_processing/06_associate_ctas_to_nodes.py
+++ spoke/data_processing/06_associate_ctas_to_nodes.py
@@ -4,31 +4,29 @@ import sys
 import osmnx as ox
 from os.path import abspath, dirname, join
 
-DIR = sys.path[0] if __name__ == '__main__' else dirname(__loader__.path)
+INPUT_FILE_GRAPH = "pipeline_data/target_map.graphml"
+INPUT_FILE_CENSUS_SHAPEFILE = "pipeline_data/raw_data/2010_Census_Tracts/geo_export_85c202c5-6ec9-493e-b0ec-a13efa26758d.shp"
+INPUT_FILE_DATASET = "pipeline_data/unified_dataset.pkl.gz"
+OUTPUT_FILE = "pipeline_data/node_id_census_tract_key.pkl.gz"
 
-module_path = abspath(join(DIR, '../..'))
-if module_path not in sys.path:
-    sys.path.append(module_path)
-
-INPUT_FILE_GRAPH = abspath(join(DIR, '../../pipeline_data/target_map.graphml'))
-INPUT_FILE_CENSUS_SHAPEFILE = abspath(join(DIR, '../../pipeline_data/raw_data/2010_Census_Tracts/geo_export_85c202c5-6ec9-493e-b0ec-a13efa26758d.shp'))
-INPUT_FILE_DATASET = abspath(join(DIR, '../../pipeline_data/unified_dataset.pkl.gz'))
-OUTPUT_FILE = abspath(join(DIR, '../../pipeline_data/node_id_census_tract_key.pkl.gz'))
 
 def process():
-  G = ox.io.load_graphml(INPUT_FILE_GRAPH)
-  tract_gdf = gpd.read_file(INPUT_FILE_CENSUS_SHAPEFILE)
+    G = ox.io.load_graphml(INPUT_FILE_GRAPH)
+    tract_gdf = gpd.read_file(INPUT_FILE_CENSUS_SHAPEFILE)
 
-  node_gdf = ox.utils_graph.graph_to_gdfs(G,nodes=True, edges=False, node_geometry=True)
+    node_gdf = ox.utils_graph.graph_to_gdfs(
+        G, nodes=True, edges=False, node_geometry=True
+    )
 
-  ny_county_tracts_gdf = tract_gdf.loc[tract_gdf['boro_name'] == 'Manhattan'].copy()
+    ny_county_tracts_gdf = tract_gdf.loc[tract_gdf["boro_name"] == "Manhattan"].copy()
 
-  nodes_in_tracts = gpd.sjoin(node_gdf, ny_county_tracts_gdf, predicate='intersects')
+    nodes_in_tracts = gpd.sjoin(node_gdf, ny_county_tracts_gdf, predicate="intersects")
 
-  node_id_census_tracts = nodes_in_tracts[['ct2010']].copy()
+    node_id_census_tracts = nodes_in_tracts[["ct2010"]].copy()
 
-  return node_id_census_tracts
+    return node_id_census_tracts
 
-if __name__ == '__main__':
-  output = process()
-  output.to_pickle(OUTPUT_FILE)
+
+if __name__ == "__main__":
+    output = process()
+    output.to_pickle(OUTPUT_FILE)
